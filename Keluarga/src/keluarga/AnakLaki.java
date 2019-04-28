d7dc258ff07b0ed27e4c3984370d426327c959aa
BrotherWood <47861193+BrotherWood@users.noreply.github.com>
2019-04-28 23:49:13 +0700

    Add files via upload

---
+++ Keluarga/src/keluarga/AnakLaki.java
@@ -0,0 +1,26 @@
+package keluarga;
+
+public class AnakLaki extends Ayah {
+	private int berat;
+	private int umur;
+	
+	public AnakLaki() {
+		this.berat=50;
+		this.umur=15;
+	}
+	
+	public int getBerat() {
+		return berat;
+	}
+	public void setBerat(int berat) {
+		this.berat = berat;
+	}
+	public int getUmur() {
+		return umur;
+	}
+	public void setUmur(int umur) {
+		this.umur = umur;
+	}
+	
+	
+}
---
+++ Keluarga/src/keluarga/Ayah.java
@@ -0,0 +1,42 @@
+package keluarga;
+
+public class Ayah {
+
+	private String muka;
+	private String hidung;
+	private String nama;
+	public Ayah()
+	{
+		this.muka= "Oval";
+		this.hidung="Mancung";
+	}
+
+	public String getHidung() {
+		return hidung;
+	}
+
+	public void setHidung(String hidung) {
+		this.hidung = hidung;
+	}
+
+	public String getNama() {
+		return nama;
+	}
+
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+
+	public String getMuka() {
+		return muka;
+	}
+
+	public void setMuka(String muka) {
+		this.muka = muka;
+	}
+
+	
+	
+	
+	
+}
